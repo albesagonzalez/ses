60c20c7db90f0cad9424aeffd9d1e0068898bfd6
albesagonzalez <albert.albesa.gonzalez@gmail.com>
2024-09-02 11:43:00 +0100

    add region sparsity

--- rec_feed_network.py
+++ rec_feed_network.py
@@ -86,6 +86,7 @@ class RFNetwork(nn.Module):
       for region_index, region in enumerate(self.in_regions):
         x_prime = torch.zeros(len(region))
         x_prime[torch.topk(x, int(len(region)*self.in_sparsity[region_index])).indices] = 1
+        print(x_prime, region_inde)
         final_x[region]  = x_prime
       return x_prime
 
