56fe622328ab6a4bbf2e1d1957268463cf27e27c
albesagonzalez <albert.albesa.gonzalez@gmail.com>
2024-09-26 07:45:28 +0100

    clean code

--- mean_field.py
+++ mean_field.py
@@ -13,13 +13,17 @@ def get_probs(latent_space, input_size, network_sparsity):
           element_j = (neuron_j - get_starting_neuron_from_attribute(attribute_j, latent_space))//latent_space.act_sizes[attribute_j]
           if attribute_i ==  attribute_j:
             if element_i != element_j:
+              print("hey 0")
               p["ij"][neuron_i, neuron_j] = 0
             if element_i == element_j:
               if attribute_i == 0:
+                print("hey 1")
                 p["ij"][neuron_i, neuron_j] = np.sum(p_ij_latent[element_i])
               else:
+                print("hey 2")
                 p["ij"][neuron_i, neuron_j] = np.sum(p_ij_latent[:, element_i])
           else:
+            print("hey 3")
             (element_0, element_1) = (element_i, element_j) if attribute_i == 0 else (element_j, element_i)
             p["ij"][neuron_i, neuron_j] = p_ij_latent[element_0, element_1]
         
